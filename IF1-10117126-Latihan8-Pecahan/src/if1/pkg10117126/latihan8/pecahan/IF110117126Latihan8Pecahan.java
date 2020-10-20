805da84c55f3e8059cf1b6912a4824c6fd93d92a
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-10-21 00:46:00 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan8-Pecahan/src/if1/pkg10117126/latihan8/pecahan/IF110117126Latihan8Pecahan.java
@@ -0,0 +1,27 @@
+/*
+ * 
+ * @author
+ * NAMA : Mawa Faqu Rochman
+ * KELAS : PBO1
+ * NIM : 10117126
+ *
+ */
+package if1.pkg10117126.latihan8.pecahan;
+
+
+public class IF110117126Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789f;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " +d);
+    }
+    
+}
